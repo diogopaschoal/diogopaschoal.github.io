be1dacaf3338148bbb519a0d44f5dcb288ee47f9
Diogo <diogo.paschoal@protonmail.com>
2022-03-14 21:30:28 -0300

    Adding docs folder.

---
+++ docs/precache-manifest.37e533b2cbb5fcbbbd62c019c1dd5adb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca92c848650d0a2b6eb",
+    "url": "/css/app.35892048.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "18bae4fbff4d5ec91a8f15c0a65587d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecd61f0b2c56ed0d42f1",
+    "url": "/js/about.b4f9a50c.js"
+  },
+  {
+    "revision": "0ca92c848650d0a2b6eb",
+    "url": "/js/app.cf6573e2.js"
+  },
+  {
+    "revision": "b0a5e5601dcb77e93098",
+    "url": "/js/chunk-vendors.bd7e8eff.js"
+  },
+  {
+    "revision": "7c1bb21dc83423f0ae9c1871342cfee7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
